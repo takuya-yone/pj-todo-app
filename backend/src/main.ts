0eab662f136e04f4bf3d6c2c4ba5a7207751a7a9
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2024-02-11 18:40:50 +0900

    Add Swagger documentation and TodoItem type

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  app.setGlobalPrefix('api')
+
+  const config = new DocumentBuilder().build()
+  const document = SwaggerModule.createDocument(app, config, {
+    ignoreGlobalPrefix: false,
+  })
+  SwaggerModule.setup('api/docs', app, document)
+
+  await app.listen(4000)
 }
 bootstrap()
---
+++ types/src/TodoItem.ts
@@ -0,0 +1,7 @@
+export type TodoItem = {
+  id: string
+  complete: boolean
+  title: string
+  startDate: Date
+  endDate: Date
+}
