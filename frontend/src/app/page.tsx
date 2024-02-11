ae1042ee2b289520cbb79c37bb7c85e0c2feee83
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2024-02-11 20:14:54 +0900

    Add CORS and SWR library, fetch todo items

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,19 @@
+'use client'
+
+import { TodoItem } from '@prisma/client'
 import { Button, Typography } from 'antd'
+import useSWR from 'swr'
+
+async function fetcher(key: string, init?: RequestInit) {
+  return fetch(key, init).then((res) => res.json() as Promise<TodoItem[]>)
+}
 
 export default function Home() {
+  const apiEndpoint = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/todo`
+  const { data: response, error } = useSWR(apiEndpoint, fetcher, {
+    revalidateOnFocus: true,
+    // refreshInterval: 3,
+  })
   return (
     <>
       <Button>dsfsafa</Button>
