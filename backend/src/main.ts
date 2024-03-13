c59abae683845bff8c525581abaeabec6798feef
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2024-03-14 02:46:45 +0900

    Add OpenAPI documentation generation to backend service

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,9 @@
+import { writeFileSync } from 'node:fs'
+import * as path from 'node:path'
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { dump } from 'js-yaml'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
@@ -14,6 +17,8 @@ async function bootstrap() {
     ignoreGlobalPrefix: false,
   })
   SwaggerModule.setup('api/docs', app, document)
+  const outputPath = path.resolve(process.cwd(), 'openapi.yml')
+  writeFileSync(outputPath, dump(document, {}))
 
   await app.listen(4000)
 }
