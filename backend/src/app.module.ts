322100c503dd15d4ae7d238f07c2712dd53d5785
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2024-03-14 02:32:07 +0900

    Update dependencies and import paths to latest versions

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,4 @@
-import { join } from 'path'
+import { join } from 'node:path'
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common'
 import { ServeStaticModule } from '@nestjs/serve-static'
 import { AppController } from './app.controller'
