03a2ac804d83298264401f2fa48ad25c9dde16ea
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2026-01-17 09:38:34 +0900

    Refactor code structure for improved readability and maintainability

--- packages/backend/src/todo/todo.controller.spec.ts
+++ packages/backend/src/todo/todo.controller.spec.ts
@@ -1,4 +1,5 @@
-import { Test, TestingModule } from '@nestjs/testing'
+import assert from 'node:assert'
+import { beforeEach, describe, expect, it } from 'vitest'
 import { PrismaService } from '../prisma/prisma.service'
 import { TodoController } from './todo.controller'
 import { CreateTodoDto, DeleteTodoDto, UpdateTodoDto } from './todo.dto'
@@ -6,16 +7,15 @@ import { TodoService } from './todo.service'
 
 describe('TodoController', () => {
   let todoController: TodoController
+  let todoService: TodoService
+  let prismaService: PrismaService
 
   const mockDto = new CreateTodoDto('titletitle', 'commentcomment')
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [TodoController],
-      providers: [TodoService, PrismaService],
-    }).compile()
-
-    todoController = app.get<TodoController>(TodoController)
+    prismaService = new PrismaService()
+    todoService = new TodoService(prismaService)
+    todoController = new TodoController(todoService)
   })
 
   describe('Todo', () => {
@@ -28,26 +28,34 @@ describe('TodoController', () => {
     it('getTodo', async () => {
       const result = await todoController.getTodo()
       expect(result.length).toBeGreaterThan(1)
+
       const last = result.pop()
+      assert(last)
       expect(last.title).toBe(mockDto.title)
       expect(last.comment).toBe(mockDto.comment)
     })
     it('updateTodo', async () => {
-      const id = (await todoController.getTodo()).pop().id
-      const mockUpdateDto = new UpdateTodoDto(id, 'titletitle2', 'commentcomment2', true)
+      const item = (await todoController.getTodo()).pop()
+      assert(item)
+
+      const mockUpdateDto = new UpdateTodoDto(item.id, 'titletitle2', 'commentcomment2', true)
       await todoController.putTodo(mockUpdateDto)
+
       const newItem = (await todoController.getTodo()).pop()
+      assert(newItem)
       expect(newItem.id).toBe(mockUpdateDto.id)
       expect(newItem.title).toBe(mockUpdateDto.title)
       expect(newItem.comment).toBe(mockUpdateDto.comment)
       expect(newItem.complete).toBe(mockUpdateDto.complete)
     })
     it('deleteTodo', async () => {
-      const id = (await todoController.getTodo()).pop().id
-      const mockDeleteDto = new DeleteTodoDto(id)
+      const item = (await todoController.getTodo()).pop()
+      assert(item)
+
+      const mockDeleteDto = new DeleteTodoDto(item.id)
       await todoController.deleteTodo(mockDeleteDto)
       const newItem = (await todoController.getTodo()).pop()
-      expect(newItem.id).not.toBe(mockDeleteDto.id)
+      expect(newItem?.id).not.toBe(mockDeleteDto.id)
     })
   })
 })
